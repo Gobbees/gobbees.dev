2d09c3f1cd9e3b8bea938b194d5758cd327b02a5
Giovanni Gobbi <ggobbi@amazon.com>
2023-03-30 18:33:58 +0100

    Fix ContentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-// You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
   default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
